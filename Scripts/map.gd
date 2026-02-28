4c66a4691f597acc23eba379b9762743189d56ea
Awesometems <119901743+Awesometems@users.noreply.github.com>
2026-02-27 23:16:59 -0500

    map show and background room 1

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -1,12 +1,28 @@
 extends Node2D
 class_name Map
 
+var layout1 = [
+	[0,0,1,0,0],
+	[0,0,0,0,1],
+	[0,1,0,0,0],
+	[0,1,0,1,0],
+	[0,1,0,0,0]
+]
 
+var cell_size := 50
+var SquareScene := preload("res://Scenes/square.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var room1 = Level.new()
-	
-	
+func _ready():
+	showmap(layout1)
+
 func showmap(layout):
-	pass
+	var ypos = 0
+	for row in layout:
+		var xpos = 0
+		for cell in row:
+			if cell == 1:
+				var sq = SquareScene.instantiate()
+				add_child(sq)
+				sq.position = Vector2(xpos, ypos)
+			xpos += cell_size
+		ypos += cell_size

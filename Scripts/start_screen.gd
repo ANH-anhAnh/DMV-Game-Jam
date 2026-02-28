0e174662f5b53000104286f5101fba7411330624
ANH-anhAnh <anhvhtruong@gmail.com>
2026-02-28 13:59:48 -0500

    Transition
    I DIDN'T break your map. or your hunger

--- Scripts/start_screen.gd
+++ Scripts/start_screen.gd
@@ -2,7 +2,7 @@ extends Node2D
 var SquareScene := preload("res://Scenes/square.tscn")
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	get_tree().change_scene_to_file("res://Scenes/rooms/room_1.tscn")
 
 func _on_options_pressed() -> void:
 	pass

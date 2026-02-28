aa88bd3a65de05b2eca43dcdf1a6fab2793c5810
Awesometems <119901743+Awesometems@users.noreply.github.com>
2026-02-27 22:22:02 -0500

    map update

---
+++ Scripts/map.gd
@@ -0,0 +1,12 @@
+extends Node2D
+class_name Map
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var room1 = Level.new()
+	
+	
+func showmap(layout):
+	pass

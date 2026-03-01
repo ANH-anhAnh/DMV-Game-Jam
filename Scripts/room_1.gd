38bd791863e623dc03ae678db5e7da93f7de1fd2
ANH-anhAnh <anhvhtruong@gmail.com>
2026-02-28 21:01:41 -0500

    a

--- Scripts/room_1.gd
+++ Scripts/room_1.gd
@@ -20,37 +20,46 @@ func _ready() -> void:
 		"*You feel a sharp pain at the back of your head. Your vision slowly fades.", 
 		"default", "default", 0
 	)
-
+	
+	# vision fades __-------------------------------------------------------------------------------
+	await wait_for_next_event(0.1)
+	var tween = get_tree().create_tween()
+	tween.tween_property($ColorRect, "modulate", Color(1, 1, 1, 1), 1)\
+	.set_trans(Tween.TRANS_QUAD)\
+	.set_ease(Tween.EASE_OUT)\
+	.from(Color(1, 1, 1, 0))
+	
 	textbox.queue_text(
 		"This one’s always been a pain in the arse, I tell ya. Inject him and get it over with.",
 		"default", "pastor_annoyed", 1
 	)
-	
-	await get_tree().create_timer(.1, false).timeout
-	while (textbox.start.text != ""):
-		await get_tree().create_timer(.1, false).timeout
-	await get_tree().create_timer(.1, false).timeout
-	var tween = get_tree().create_tween()
-	tween.tween_property(pastor_sprite, "modulate", Color(1, 1, 1, 1), 0.5)\
+	# pastor appears -------------------------------------------------------------------------------
+	await wait_for_next_event(0.1)
+	var tween2 = get_tree().create_tween()
+	tween2.tween_property(pastor_sprite, "modulate", Color(1, 1, 1, 1), 1)\
 	.set_trans(Tween.TRANS_QUAD)\
 	.set_ease(Tween.EASE_OUT)\
 	.from(Color(1, 1, 1, 0))
 
-
-	# wait X amount of seconds after pastor is annoyed. makes room for pastor's speech	
-	await get_tree().create_timer(.5, false).timeout
-	while (textbox.start.text != ""):
-		await get_tree().create_timer(.1, false).timeout
-	await get_tree().create_timer(2, false).timeout
-	
 	# pastor speech --------------------------------------------------------------------------------
-	pastor_speech_text.text = "Hah. Pick’s always been a rowdy one. Big ideas. Big dreams..."
-	pastor_speech_text.text = "But now, is the era of Darkness. We need unity to survive. 
-		So I beg you, children. Serve the Body faithfully, and it will serve you back."
-	pastor_speech_text.text = "For your fathers, lovers, mothers. For me. For you."
+	# TODO: animate this
+	#pastor_speech_text.text = "Hah. Pick’s always been a rowdy one. Big ideas. Big dreams..."
+	#pastor_speech_text.text = "But now, is the era of Darkness. We need unity to survive. 
+		#So I beg you, children. Serve the Body faithfully, and it will serve you back."
+	#pastor_speech_text.text = "For your fathers, lovers, mothers. For me. For you."
 	
 	# ----------------------------------------------------------------------------------------------
+	# wait X amount of seconds after pastor is annoyed. makes room for pastor's speech	
+	await wait_for_next_event(4)
+	
 	textbox.queue_text("Easy now. Don’t resist.", "default", "default", 0)
 	
 	textbox.queue_text("*You feel a pinching sensation in your arm.", "default", "default", 0)
 	textbox.queue_text("Suddenly, you jolt up and push past the pastor, running towards the exit.", "default", "default", 0)
+	
+# --------------------------------------------------------------------------------------------------
+func wait_for_next_event(time : float):
+	await get_tree().create_timer(.1, false).timeout
+	while (textbox.start.text != ""):
+		await get_tree().create_timer(.1, false).timeout
+	await get_tree().create_timer(time, false).timeout
